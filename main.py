e143167d74fe8d5f316d9a2c75919c40d64b438f
corkumb <157167891+corkumb@users.noreply.github.com>
2024-04-21 23:22:40 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,23 +2,12 @@ import sys
 from RecipeUI import RecipeUI
 from PyQt5.QtWidgets import QApplication
 from recipe import Recipe
+from RecipeProcessor import RecipeProcessor
 from json import loads
-def parse_json(filename):
-    recipes = []
-    with open(filename,'r', encoding='utf-8') as file:
-        data = loads(file.read())
-    for recipe in data:
-        name = recipe["name"]
-        description = recipe["description"]
-        image_url = recipe["image"]
-        recipe_yield = recipe["recipeYield"]
-        cook_time = recipe["cookTime"]
-        prep_time = recipe["prepTime"]
-        ingredients = recipe["ingredients"]
-        recipes.append(Recipe(name,description,image_url,recipe_yield,cook_time,prep_time,ingredients))
-    return recipes
 def main():
-    master_list = parse_json("recipes.json")
+    RecipeLoader = RecipeProcessor
+    RecipeLoader.load__recipes('recipes.json')
+    master_list = RecipeLoader.get__recipes()
     app = QApplication(sys.argv)
     gui = RecipeUI(master_list)
     gui.show()
